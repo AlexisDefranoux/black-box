64610db72557bd0d3258657f5c6faddaaa0e2853
AlexisDefranoux <alexisdefranoux@gmail.com>
2020-02-04 17:11:33 +0100

    update interface on orchestrateur

--- front-end/src/app/shared/models/profiles.ts
+++ front-end/src/app/shared/models/profiles.ts
@@ -2,9 +2,9 @@ import {Profile} from './profile';
 
 export class Profiles {
   static readonly list = [
-    new Profile({name: 'Default', minCo2: 0, maxCo2: 7, checkFall: false, minTemperature: 20, maxTemperature: 40, minSound: 0, maxSound: 800}),
-    new Profile({name: 'Mountain', minCo2: 0, maxCo2: 6, checkFall: true, minTemperature: 0, maxTemperature: 35, minSound: 0, maxSound: 700}),
-    new Profile({name: 'Old person', minCo2: 0, maxCo2: 5, checkFall: true, minTemperature: 20, maxTemperature: 30, minSound: 0, maxSound: 600}),
-    new Profile({name: 'Fireman', minCo2: 0, maxCo2: 8, checkFall: false, minTemperature: 10, maxTemperature: 40, minSound: 0, maxSound: 1000}),
+    new Profile({name: 'Default', minCo2: 0, maxCo2: 25, minTemperature: 20, maxTemperature: 30, minSound: 0, maxSound: 400}),
+    new Profile({name: 'Mountain', minCo2: 0, maxCo2: 6, minTemperature: 0, maxTemperature: 35, minSound: 0, maxSound: 700}),
+    new Profile({name: 'Old person', minCo2: 0, maxCo2: 5, minTemperature: 20, maxTemperature: 30, minSound: 0, maxSound: 600}),
+    new Profile({name: 'Fireman', minCo2: 0, maxCo2: 8, minTemperature: 10, maxTemperature: 40, minSound: 0, maxSound: 1000}),
   ];
 }
